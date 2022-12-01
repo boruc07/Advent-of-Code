5874f99c8cd99ddb062471db29959be32ea4d7a9
boruc07 <marcin.borucinski@gmail.com>
2022-12-01 22:40:09 +0100

    removed unused variable

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,5 @@
 # Input text
-with open("input.txt", 'rt') as f:
+with open("day1/input.txt", 'rt') as f:
     input = f.read()
 
 # Split into items
@@ -7,14 +7,12 @@ input_list = input.split("\n")
 
 # Iterate through items if \n then it's another elf
 sum_list = []
-iterator = 0
 sum_value = 0
 
 for item in input_list:
     if item != "":
         sum_value += int(item)
     else:
-        iterator += 1
         sum_list.append(sum_value)
         sum_value = 0
 
